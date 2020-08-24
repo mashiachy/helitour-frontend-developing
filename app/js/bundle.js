cb19eebe7c5ff2e2c21daf902374baf78c4e0284
mashiachy <38632062+mashiachy@users.noreply.github.com>
2020-08-24 20:49:31 +0500

    404 article articles faq offer done

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,7 +1,1 @@
-const vhFix = () => {
-  const setVh = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
-  setVh();
-  window.addEventListener('resize', setVh);
-};
 
-vhFix();
